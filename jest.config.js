f23200320ee998140b893960085ee31268a9fc9a
Serban Stancu <stancu@adobe.com>
2020-12-03 10:16:38 -0700

    Cleanup.

--- jest.config.js
+++ jest.config.js
@@ -18,14 +18,5 @@ module.exports = {
       runner: 'jest-runner-eslint',
       testMatch: ['<rootDir>/src/**']
     }
-  ],
-
-  collectCoverageFrom: [
-    './src/**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-    '!**/__tests__/**'
-  ],
-
-  coverageReporters: ['lcov', 'text', 'html']
+  ]
 };
